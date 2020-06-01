825d21433b9a1168957a16b41bd745cae491c8e9
Marina Kun <mk3qa@virginia.edu>
2020-06-01 16:12:29 -0400

    Updated text

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,30 +21,18 @@ export default () => (
       </div>*/}
       <div className="container">
         <Fade duration={1500} delay={200}>
-          <HomeCard title="Business Plan" graphic={BusinessPlanSVG} sectionId={1}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Nostrum nobis architecto quas ratione ad animi laudantium,
-            neque rem consequuntur suscipit saepe natus impedit. Aut minima eos
-            quibusdam consequatur quidem vitae suscipit modi magni tempora rem,
-            voluptatibus quam dolorem incidunt aspernatur!
+          <HomeCard title="Mission Statement" graphic={BusinessPlanSVG} sectionId={1}>
+            To provide a creative space for motivated students at the University of Virginia to craft data-driven solutions in response to challenges faced by organizations in the University and Charlottesville communities
           </HomeCard>
         </Fade>
         <Fade duration={1500} delay={200}>
-          <HomeCard title="Data Analytics" graphic={DataProcessingSVG} sectionId={2}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Nostrum nobis architecto quas ratione ad animi laudantium,
-            neque rem consequuntur suscipit saepe natus impedit. Aut minima eos
-            quibusdam consequatur quidem vitae suscipit modi magni tempora rem,
-            voluptatibus quam dolorem incidunt aspernatur!
+          <HomeCard title="Vision" graphic={DataProcessingSVG} sectionId={2}>
+            It is our belief at HooThinks that consulting, at its core, is the act of helping others to solve challenges in a structured manner.  We want to inspire generations of UVA students with this message, allowing them to use their creativity and unique talents to affect positive change in the University community, in Charlottesville, and beyond.
           </HomeCard>
         </Fade>
         <Fade duration={1500} delay={200}>
-          <HomeCard title="Presentations" graphic={PitchingSVG} sectionId={3}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Nostrum nobis architecto quas ratione ad animi laudantium,
-            neque rem consequuntur suscipit saepe natus impedit. Aut minima eos
-            quibusdam consequatur quidem vitae suscipit modi magni tempora rem,
-            voluptatibus quam dolorem incidunt aspernatur!
+          <HomeCard title="Achieving Our Mission" graphic={PitchingSVG} sectionId={3}>
+            In order for students to develop effective data-driven solutions, HooThinks Consulting teaches them how to leverage data analysis, design thinking, project management knowledge, and consulting techniques.
           </HomeCard>
         </Fade>
       </div>
